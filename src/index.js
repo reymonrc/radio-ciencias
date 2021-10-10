818891465b680eeeb7b17813dca8b75875f63d34
Ramon RC <ramon@ramonrc.com>
2021-10-10 13:35:37 -0500

    sin imports

--- src/index.js
+++ src/index.js
@@ -7,21 +7,18 @@ console.log("🦊 Hello! Edit me in src/index.js");
 
 import MotionMachine from "./js/smoke.js";
 
-//import "./js/local.js";
-//import "./js/audio.js";
 
-
-			var no_wr3ckords = 24,
-			    no_images = 5,
+			var no_wr3ckords = 1,
+			    no_images = 1,
 			    image = 0,
 			    play = 0;
 
-			function setup_audio_continuer()
+function setup_audio_continuer()
 			{
 				var myAudio = document.getElementById('my_audio');
 				myAudio.onended = function()
 				{
-					myAudio.src = "./mp3/wr3ckord_".concat((new Date().getSeconds())%no_wr3ckords + 1).concat(".mp3");
+					myAudio.src = "./mp3/wr3ckord_2.mp3";
 					myAudio.play();
 				}
 			}
@@ -35,7 +32,7 @@ import MotionMachine from "./js/smoke.js";
 						image = no_images-1;
 					else
 						--image;
-					pic.src = "./media/wr3ckord_".concat(image%no_images+1).concat(".jpg");
+					pic.src = "./img/jigs4w.jpeg";
 				}
 
 				return;
@@ -47,7 +44,7 @@ import MotionMachine from "./js/smoke.js";
 				{
 					var pic = document.getElementById('my_media');
 					image = (image+1)%no_images;
-					pic.src = "./media/wr3ckord_".concat(image%no_images+1).concat(".jpg");
+					pic.src = "./img/jigs4w.jpeg";
 				}
 
 				return;
@@ -93,7 +90,7 @@ import MotionMachine from "./js/smoke.js";
 						image = no_images-1;
 					else
 						--image;
-					pic.src = "./media/wr3ckord_".concat(image%no_images+1).concat(".jpg");
+					pic.src = "./img/jigs4w.jpeg";
 
 					return;
 				}
@@ -108,13 +105,13 @@ import MotionMachine from "./js/smoke.js";
 				{
 					var pic = document.getElementById('my_media');
 					image = (image+1)%no_images;
-					pic.src = "./media/wr3ckord_".concat(image%no_images+1).concat(".jpg");
+					pic.src = "./img/jigs4w.jpeg";
 
 					return;
 				}
 
 				var myAudio = document.getElementById('my_audio');
-				myAudio.src = "./mp3/wr3ckord_".concat(wr3ckord%no_wr3ckords+1).concat(".mp3");
+				myAudio.src = "./mp3/wr3ckord_2.mp3";
 				myAudio.play();
 			}
 
@@ -123,18 +120,19 @@ import MotionMachine from "./js/smoke.js";
 				return new Promise(resolve => setTimeout(resolve, ms));
 			}
 
-			function play_tune()
+function play_tune()
 			{
-				document.getElementById("character").src = "./imgs/logo_".concat((new Date().getSeconds())%11 + 1).concat(".png");
+				document.getElementById("character").src = "./img/jigs4w.jpeg";
 				play = 1;
 
 				var myAudio = document.getElementById('my_audio');
-				myAudio.src = "./mp3/wr3ckord_".concat((new Date().getSeconds())%no_wr3ckords + 1).concat(".mp3");
+				myAudio.src = "./mp3/wr3ckord_2.mp3";
 				myAudio.play();
 			}
-			function toggle_banner()
+
+function toggle_banner()
 			{
-				document.getElementById("wr3cked").src = "./imgs/banner_".concat((new Date().getSeconds())%11 + 1).concat(".png");
+				document.getElementById("wr3cked").src = "./img/jigs4w.jpeg";
 			}
 
 			var canvas = document.getElementById('canvas')
@@ -174,7 +172,7 @@ import MotionMachine from "./js/smoke.js";
 				return milisec_diff;
 			}
 
-			onmousemove = function (e)
+            onmousemove = function (e)
 			{
 				if(get_time_diff(datetime) > colTime)
 				{
@@ -182,7 +180,7 @@ import MotionMachine from "./js/smoke.js";
 					c2 = Math.floor(Math.random() * 250);
 					c3 = Math.floor(Math.random() * 250);
 					party.stop();
-					party = motionmachine(ctx, [c1, c2, c3]);
+					party = MotionMachine(ctx, [c1, c2, c3]);
 					party.start(); // start animating
 					datetime = new Date().getTime();
 					type = Math.floor(Math.random() * 5);
