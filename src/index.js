d21f6a4c368a52dff92d376141ed7fc00c7ff715
Ramon RC <ramon@ramonrc.com>
2021-10-10 12:45:28 -0500

    error

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ console.log("🦊 Hello! Edit me in src/index.js");
 
 import MotionMachine from "./js/smoke.js";
 
-import onmousemove from "./js/local.js";
+// import onmousemove from "./js/local.js";
 import setup_audio_continuer from "./js/audio.js";
