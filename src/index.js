6b8d52134ff087266ec64b6b1c499ef76ab523b2
Ramon RC <ramon@ramonrc.com>
2021-10-10 10:31:45 -0500

    define

--- src/index.js
+++ src/index.js
@@ -7,4 +7,9 @@ console.log("🦊 Hello! Edit me in src/index.js");
 
 import MotionMachine from "./smoke.js";
 
-MotionMachine();
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+canvas.width = innerWidth
+canvas.height = innerHeight
+// smokemachine(context, [r,g,b])
+var demo = MotionMachine(ctx, [54, 16.8, 18.2])
