5922228c509d22071bed2f6526e944df411d58a2
Ramon RC <ramon@ramonrc.com>
2021-10-10 09:05:17 -0500

    Try smoke 1/2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.css";
-import "./css/pagestyle.css";
+//import "./css/pagestyle.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
 
-import "smoke.js";
+import "./smoke.js";
